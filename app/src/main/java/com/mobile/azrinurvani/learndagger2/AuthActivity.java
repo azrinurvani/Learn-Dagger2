b2149261d74778ecc4d4b7e48fa42ef506e89e80
azrinurvani <azrinurvani@gmail.com>
2021-04-21 15:27:20 +0700

    Step 4 - Inject Glide pada AppModule 1. Hapus dummy depedency yang ada di AppModule, begitupun @Inject pada AuthActivity 2. Copy paste design layout activity_auth.xml 3. Buat method dengan annotaion @Provides yang bernama provideRequestOptions() dengan nilai return RequestOptions di AppModule 4. Buat method dengan annotaion @Provides yang bernama provideGlideInstance dengan nilai return RequestManager yang memiliki parameter application dan requestOptions di AppModule 5. Buat method (depedency) dengan annotation @Provides yang bernama provideAppDrawable dengan return Drawable (untuk set Image pada Glide berupa logo)

--- app/src/main/java/com/mobile/azrinurvani/learndagger2/AuthActivity.java
+++ app/src/main/java/com/mobile/azrinurvani/learndagger2/AuthActivity.java
@@ -3,8 +3,13 @@ package com.mobile.azrinurvani.learndagger2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Application;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
 
 import javax.inject.Inject;
 
@@ -15,22 +20,34 @@ public class AuthActivity extends DaggerAppCompatActivity {
     private static final String TAG = "AuthActivity";
 
 //    Syarat agar bisa inject dari suatu object mesti ada provides atau bind di module dan module tersebut di daftar di AppComponent
-    @Inject
-    String testString;
+//    @Inject
+//    String testString;
 
     //Contoh Inject Tipe Application
     // Application bisa di inject pada AuthActivity dikarenakan sudah memiliki BindInstance di AppComponent dan disediakan
     // annotation @Provide pada AppModule
     // Yang diinject harus sesuai dengan tipe data atau jenis dari object tersebut
+//    @Inject
+//    boolean isAppNull;
+
     @Inject
-    boolean isAppNull;
+    RequestManager requestManager;
+
+    @Inject
+    Drawable logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+//        Log.d(TAG, "onCreate: String ->"+testString);
+//        Log.d(TAG, "onCreate: IsAppNull ? "+isAppNull);
 
-        Log.d(TAG, "onCreate: String ->"+testString);
-        Log.d(TAG, "onCreate: IsAppNull ? "+isAppNull);
+        setLogo();
+    }
+
+    private void setLogo(){
+        requestManager.load(logo)
+                .into((ImageView)findViewById(R.id.login_logo));
     }
 }
--- app/src/main/java/com/mobile/azrinurvani/learndagger2/di/AppModule.java
+++ app/src/main/java/com/mobile/azrinurvani/learndagger2/di/AppModule.java
@@ -1,21 +1,47 @@
 package com.mobile.azrinurvani.learndagger2.di;
 
 import android.app.Application;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.mobile.azrinurvani.learndagger2.R;
 
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppModule {
-
-//    Pada setiap depedency mesti dibikin static karena itu rekomendasi dari Dokumentasi dari Dagger2
+    //    Pada setiap depedency mesti dibikin static karena itu rekomendasi dari Dokumentasi dari Dagger2
     @Provides
-    static String someString(){
-        return "this is test string from inject";
+    static RequestOptions provideRequestOptions(){
+        return RequestOptions.placeholderOf(R.drawable.white_background)
+                .error(R.drawable.white_background);
+    }
+
+    // Untuk parameter application dan requestOptions bisa diakses dikarenakan sudah di @Provides atau di BindsInstance sebelumnya
+    @Provides
+    static RequestManager provideGlideInstance(Application application,RequestOptions requestOptions){
+        return Glide.with(application)
+                .setDefaultRequestOptions(requestOptions);
     }
 
     @Provides
-    static boolean getApp(Application application){
-        return application == null;
+    static Drawable provideAppDrawable(Application application){
+        return ContextCompat.getDrawable(application,R.drawable.logo);
     }
+
+
+//    @Provides
+//    static String someString(){
+//        return "this is test string from inject";
+//    }
+//
+//    @Provides
+//    static boolean getApp(Application application){
+//        return application == null;
+//    }
 }
