265586d0f91d116f9cca6cd7a61c6a70efd799a7
azrinurvani <azrinurvani@gmail.com>
2021-04-23 20:38:23 +0700

    Step 8 - Convert Rx Flowable to LiveData using LiveDataReactiveStreams 1. Hapus blok program pemanggilan authApi.getUser pada AuthViewModel 2. Buat object authUser dengan return MediatorLiveData<User> pada AuthViewModel 3. Buat method dengan nama observeUser() dengan return authUser pada AuthViewModel 4. Buat method authenticateWithId(int userId) untuk proses convert Rx Flowable ke LiveData 5. Panggil method tersebut setelah action listener dari login button di AuthActivity 6. Run App

--- app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/auth/AuthActivity.java
+++ app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/auth/AuthActivity.java
@@ -1,24 +1,29 @@
 package com.mobile.azrinurvani.learndagger2.ui.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.app.Application;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 import com.mobile.azrinurvani.learndagger2.R;
+import com.mobile.azrinurvani.learndagger2.models.User;
 import com.mobile.azrinurvani.learndagger2.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class AuthActivity extends DaggerAppCompatActivity {
+public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "AuthActivity";
 
@@ -44,20 +49,56 @@ public class AuthActivity extends DaggerAppCompatActivity {
 
     private AuthViewModel viewModel;
 
+    private EditText userId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 //        Log.d(TAG, "onCreate: String ->"+testString);
 //        Log.d(TAG, "onCreate: IsAppNull ? "+isAppNull);
+        userId = findViewById(R.id.user_id_input);
+
+        findViewById(R.id.login_button).setOnClickListener(this);
 
         viewModel = ViewModelProviders.of(this,providerFactory).get(AuthViewModel.class);
 
         setLogo();
+        subscribeObserver();
     }
 
     private void setLogo(){
         requestManager.load(logo)
                 .into((ImageView)findViewById(R.id.login_logo));
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case  R.id.login_button : {
+                attemptingLogin();
+                break;
+            }
+        }
+    }
+
+    private void attemptingLogin() {
+        if (TextUtils.isEmpty(userId.getText().toString())){
+            return;
+        }
+        viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()));
+    }
+
+    private void subscribeObserver(){
+        viewModel.observeUser().observe(this, new Observer<User>() {
+
+            @Override
+            public void onChanged(User user) {
+                if (user !=null){
+                    Log.d(TAG, "onChanged: "+user.getEmail());
+                }
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/auth/AuthViewModel.java
@@ -2,16 +2,16 @@ package com.mobile.azrinurvani.learndagger2.ui.auth;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.mobile.azrinurvani.learndagger2.di.network.auth.AuthApi;
 import com.mobile.azrinurvani.learndagger2.models.User;
 
 import javax.inject.Inject;
-
-import io.reactivex.Observer;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
@@ -19,34 +19,34 @@ public class AuthViewModel extends ViewModel {
     private static final String TAG = "AuthViewModel";
 
     private AuthApi authApi;
+
+    private MediatorLiveData<User> authUser = new MediatorLiveData<>();
+
     @Inject
     public AuthViewModel(AuthApi authApi) {
         this.authApi = authApi;
         Log.d(TAG, "AuthViewModel: viewModel is Working....");
 
-        authApi.getUser(1)
-                .toObservable()
+
+    }
+
+    public void authenticateWithId(int userId){
+        //LiveDataReactiveStreams berfungsi untuk convert Rx Observables (Flowable) ke dalam bentuk LiveData
+        final LiveData<User> source = LiveDataReactiveStreams.fromPublisher(
+                authApi.getUser(userId)
                 .subscribeOn(Schedulers.io())
-                .subscribe(new Observer<User>() {
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
+        );
 
-                    }
+        authUser.addSource(source, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                authUser.setValue(user);
+                authUser.removeSource(source);
+            }
+        });
+    }
 
-                    @Override
-                    public void onNext(@NonNull User user) {
-                        Log.d(TAG, "onNext: "+user.getEmail());
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        Log.e(TAG, "onError: ",e );
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
+    public LiveData<User> observeUser(){
+        return authUser;
     }
 }
