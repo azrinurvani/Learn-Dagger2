daeae2891ed4877f64b843e008d4057422361ed8
azrinurvani <azrinurvani@gmail.com>
2021-04-26 15:04:07 +0700

    Step 12 - Dagger2 Fragment and ViewModels 1. Pada ProfileViewModel buat method untuk getAuthUser dari SessionManager dengan return LiveData<AuthResource<User>> 2. Tambahkan object sessionManager di constructor ProfileViewModel 3. Panggil method tersebut di ProfileFragment dan buat method untuk handle tersebut dengan nama subscribeObservers() 4. Buat method untuk set detail TextView (name,email,website) dan set Error dari TextView yang ada di ProfileFragment tersebut 5. Run app

--- app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/main/profile/ProfileFragment.java
+++ app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/main/profile/ProfileFragment.java
@@ -5,13 +5,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.mobile.azrinurvani.learndagger2.R;
+import com.mobile.azrinurvani.learndagger2.models.User;
+import com.mobile.azrinurvani.learndagger2.ui.auth.AuthResource;
 import com.mobile.azrinurvani.learndagger2.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
@@ -27,6 +31,8 @@ public class ProfileFragment extends DaggerFragment {
     @Inject
     ViewModelProviderFactory viewModelProviderFactory;
 
+    private TextView name,email,website;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -37,7 +43,49 @@ public class ProfileFragment extends DaggerFragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Log.d(TAG, "onViewCreated: ProfileFragment was created...");
+
+        name = view.findViewById(R.id.username);
+        email = view.findViewById(R.id.email);
+        website = view.findViewById(R.id.website);
         viewModel = ViewModelProviders.of(this,viewModelProviderFactory).get(ProfileViewModel.class);
-        //super.onViewCreated(view, savedInstanceState);
+
+        subscribeObservers();
+    }
+
+    private void subscribeObservers(){
+        //Pada fragment mesti removeObservers terlebih dahulu dengan parameter getViewLifecycleOwner
+        //karena pada Fragment memiliki lifecycle yang berbeda dengan activity baik berupa onDestroy, reCreated dan semacamnya pada android system
+        // sehingga untuk memastikan apakah observer sebelumnya di hapus maka harus melakukan tahap ini atau menggunakan method
+        // removeObservers(getViewLifecycleOwner) di Fragment sebelum di assign ke data lain
+        viewModel.getAuthenticateUser().removeObservers(getViewLifecycleOwner());
+        viewModel.getAuthenticateUser().observe(getViewLifecycleOwner(), new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if (userAuthResource!=null){
+                    switch (userAuthResource.status){
+                        case AUTHENTICATED:{
+                            setUserDeatails(userAuthResource.data);
+                            break;
+                        }
+                        case ERROR:{
+                            setErrorDetails(userAuthResource.message);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void setErrorDetails(String message) {
+        name.setText(message);
+        email.setText("Error");
+        website.setText("Error");
+    }
+
+    private void setUserDeatails(User data) {
+        name.setText(data.getName());
+        email.setText(data.getEmail());
+        website.setText(data.getWebsite());
     }
 }
--- app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/main/profile/ProfileViewModel.java
+++ app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/main/profile/ProfileViewModel.java
@@ -2,7 +2,12 @@ package com.mobile.azrinurvani.learndagger2.ui.main.profile;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.mobile.azrinurvani.learndagger2.SessionManager;
+import com.mobile.azrinurvani.learndagger2.models.User;
+import com.mobile.azrinurvani.learndagger2.ui.auth.AuthResource;
 
 import javax.inject.Inject;
 
@@ -10,8 +15,15 @@ public class ProfileViewModel extends ViewModel {
 
     private static final String TAG = "ProfileViewModel";
 
+    private final SessionManager sessionManager;
+
     @Inject
-    public ProfileViewModel() {
+    public ProfileViewModel(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
         Log.d(TAG, "ProfileViewModel: viewModel is working...");
+    }
+
+    public LiveData<AuthResource<User>> getAuthenticateUser(){
+        return sessionManager.getAuthUser();
     }
 }
