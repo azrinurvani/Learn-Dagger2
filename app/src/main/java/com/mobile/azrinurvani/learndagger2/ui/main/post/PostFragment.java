054c158b7f5a4272bb2030e74e9c69b11f5eb772
azrinurvani <azrinurvani@gmail.com>
2021-04-27 17:02:38 +0700

    Step 14 - RxJava Query in ViewModel with SubComponent Depedency 1. Buat class baru dengan nama Resource<T> (class ini berfungsi untuk generic model dari response end point) 2. Copy paste dari GIT untuk Resource<T> 3. Pada PostViewModel buat object untuk mediator live data 4. Pada PostViewModel buat method untuk return LiveData dari end point post (mainApi.getPostFromUser(id)) dengan nama 4. Panggil method tersebut di PostFragment dengan membuat method baru yang bernama subscribeObservers() 5. Panggil method subscribeObservers() di onViewCreated() 6. Run App

--- app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/main/post/PostFragment.java
+++ app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/main/post/PostFragment.java
@@ -1,6 +1,7 @@
 package com.mobile.azrinurvani.learndagger2.ui.main.post;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,11 +9,16 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mobile.azrinurvani.learndagger2.R;
+import com.mobile.azrinurvani.learndagger2.models.Post;
+import com.mobile.azrinurvani.learndagger2.ui.Resource;
 import com.mobile.azrinurvani.learndagger2.viewmodels.ViewModelProviderFactory;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -40,5 +46,17 @@ public class PostFragment extends DaggerFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         recyclerView = view.findViewById(R.id.recycler_view);
         viewModel = ViewModelProviders.of(this,viewModelProviderFactory).get(PostViewModel.class);
+        subscribeObservers();
+    }
+
+
+    private void subscribeObservers(){
+        viewModel.observePosts().removeObservers(getViewLifecycleOwner());
+        viewModel.observePosts().observe(getViewLifecycleOwner(), new Observer<Resource<List<Post>>>() {
+            @Override
+            public void onChanged(Resource<List<Post>> listResource) {
+                Log.d(TAG, "onChanged: "+listResource.data);
+            }
+        });
     }
 }
--- app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/main/post/PostViewModel.java
+++ app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/main/post/PostViewModel.java
@@ -2,12 +2,25 @@ package com.mobile.azrinurvani.learndagger2.ui.main.post;
 
 import android.util.Log;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.LiveDataReactiveStreams;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
 import com.mobile.azrinurvani.learndagger2.SessionManager;
 import com.mobile.azrinurvani.learndagger2.di.network.main.MainApi;
+import com.mobile.azrinurvani.learndagger2.models.Post;
+import com.mobile.azrinurvani.learndagger2.ui.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
+
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class PostViewModel extends ViewModel {
 
@@ -16,11 +29,58 @@ public class PostViewModel extends ViewModel {
 
     private static final String TAG = "PostViewModel";
 
+    private MediatorLiveData<Resource<List<Post>>> postMediatorLiveData;
+
     @Inject
     public PostViewModel(SessionManager sessionManager, MainApi mainApi) {
         this.sessionManager = sessionManager;
         this.mainApi = mainApi;
         Log.d(TAG, "PostViewModel: viewModel is working...");
+    }
+
+    public LiveData<Resource<List<Post>>> observePosts(){
+        if (postMediatorLiveData==null){
+            postMediatorLiveData = new MediatorLiveData<>();
+            postMediatorLiveData.setValue(Resource.loading(null));
+
+            final LiveData<Resource<List<Post>>> source = LiveDataReactiveStreams.fromPublisher(
+                mainApi.getPostsFromUser(sessionManager.getAuthUser().getValue().data.getId())
+                    .onErrorReturn(new Function<Throwable, List<Post>>() {
+                        @NonNull
+                        @Override
+                        public List<Post> apply(@NonNull Throwable throwable) throws Exception {
+                            Log.e(TAG, "apply: ", throwable);
+                            Post post = new Post();
+                            post.setId(-1);
+                            ArrayList<Post> postArrayList =new ArrayList<>();
+                            postArrayList.add(post);
+                            return postArrayList;
+                        }
+                    })
+                    .map(new Function<List<Post>, Resource<List<Post>>>() {
+                        @NonNull
+                        @Override
+                        public Resource<List<Post>> apply(@NonNull List<Post> posts) throws Exception {
+                            if (posts.size()>0){
+                                if (posts.get(0).getId()==-1){
+                                    return Resource.error("Something went wrong...",null);
+                                }
+                            }
+                            return Resource.success(posts);
+                        }
+                    })
+                    .subscribeOn(Schedulers.io())
+            );
+
+            postMediatorLiveData.addSource(source, new Observer<Resource<List<Post>>>() {
+                @Override
+                public void onChanged(Resource<List<Post>> listResource) {
+                    postMediatorLiveData.setValue(listResource);
+                    postMediatorLiveData.removeSource(source);
+                }
+            });
+        }
+        return postMediatorLiveData;
     }
 
 }
