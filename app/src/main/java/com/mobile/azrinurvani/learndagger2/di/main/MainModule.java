95dd5337404d7c44931fa34b1bf4a3eafe6d6704
azrinurvani <azrinurvani@gmail.com>
2021-05-01 11:51:36 +0700

    Step 15 - RecyclerView Adapter with Dagger2 1. Buat new layout dengan nama layout_post_item.xml dan copy paste design layout tersebut dari github 2. Pada pacakge ui/main buat package baru dengan nama adapter, dan disana buatlah new class dengan nama PostRecyclerAdapter 3. Copy paste baris code untuk adapter dari recycler layout_post_item.xml dari github 4. Pada util buatlah class baru untuk item decoration dari RecyclerView dengan nama VerticalItemDecoration dan copy paste baris kode dari github ke class tersebut. 5. Tambahkan depedency untuk PostRecyclerAdapter, VerticalItemDecoration, serta LayoutManager di MainModule agar bisa di inject pada PostFragment nantinya 6. Inject PostRecyclerAdapter, VerticalItemDecoration dan LayoutManager di PostFragment 7. Buat method initRecycler untuk inisialisasi view recyclerView (adapter, decoration, dan layout manager) 8. Panggil method tersebut di onViewCreated() 9. Update baris code untuk subscribeObservers() dengan menggunakan switch statement dari status berupa LOADING, SUCCESS, dan ERROR. 10. Buat log debug pada setiap statement, dan pada case SUCCESS, set nilai adapter dari PostRecyclerAdapter, dengan method adapter.setPosts(listResources.data) 11. Run app.

--- app/src/main/java/com/mobile/azrinurvani/learndagger2/di/main/MainModule.java
+++ app/src/main/java/com/mobile/azrinurvani/learndagger2/di/main/MainModule.java
@@ -1,6 +1,12 @@
 package com.mobile.azrinurvani.learndagger2.di.main;
 
+import android.app.Application;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import com.mobile.azrinurvani.learndagger2.di.network.main.MainApi;
+import com.mobile.azrinurvani.learndagger2.ui.main.post.adapter.PostRecyclerAdapter;
+import com.mobile.azrinurvani.learndagger2.util.VerticalSpaceItemDecoration;
 
 import dagger.Module;
 import dagger.Provides;
@@ -12,5 +18,20 @@ public class MainModule {
     @Provides
     static MainApi provideMainApi(Retrofit retrofit){
         return retrofit.create(MainApi.class);
+    }
+
+    @Provides
+    static PostRecyclerAdapter providePostRecyclerAdapter(){
+        return new PostRecyclerAdapter();
+    }
+
+    @Provides
+    static LinearLayoutManager provideLinearLayoutManager(Application application){
+        return new LinearLayoutManager(application);
+    }
+
+    @Provides
+    static VerticalSpaceItemDecoration provideVerticalItemDecoration(){
+        return new VerticalSpaceItemDecoration(15);
     }
 }
--- app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/main/post/PostFragment.java
+++ app/src/main/java/com/mobile/azrinurvani/learndagger2/ui/main/post/PostFragment.java
@@ -11,11 +11,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mobile.azrinurvani.learndagger2.R;
 import com.mobile.azrinurvani.learndagger2.models.Post;
 import com.mobile.azrinurvani.learndagger2.ui.Resource;
+import com.mobile.azrinurvani.learndagger2.ui.main.post.adapter.PostRecyclerAdapter;
+import com.mobile.azrinurvani.learndagger2.util.VerticalSpaceItemDecoration;
 import com.mobile.azrinurvani.learndagger2.viewmodels.ViewModelProviderFactory;
 
 import java.util.List;
@@ -34,6 +37,14 @@ public class PostFragment extends DaggerFragment {
     private PostViewModel viewModel;
     private RecyclerView recyclerView;
 
+    @Inject
+    PostRecyclerAdapter adapter;
+
+    @Inject
+    LinearLayoutManager linearLayoutManager;
+
+    @Inject
+    VerticalSpaceItemDecoration decoration;
 
     @Nullable
     @Override
@@ -47,6 +58,7 @@ public class PostFragment extends DaggerFragment {
         recyclerView = view.findViewById(R.id.recycler_view);
         viewModel = ViewModelProviders.of(this,viewModelProviderFactory).get(PostViewModel.class);
         subscribeObservers();
+        initRecycler();
     }
 
 
@@ -55,8 +67,28 @@ public class PostFragment extends DaggerFragment {
         viewModel.observePosts().observe(getViewLifecycleOwner(), new Observer<Resource<List<Post>>>() {
             @Override
             public void onChanged(Resource<List<Post>> listResource) {
-                Log.d(TAG, "onChanged: "+listResource.data);
+                switch (listResource.status){
+                    case LOADING:{
+                        Log.d(TAG, "onChanged: Loading...");
+                        break;
+                    }
+                    case SUCCESS:{
+                        Log.d(TAG, "onChanged: Success got POST DATA");
+                        adapter.setPosts(listResource.data);
+                        break;
+                    }
+                    case ERROR:{
+                        Log.e(TAG, "onChanged: Error : "+listResource.message);
+                        break;
+                    }
+                }
             }
         });
+    }
+
+    private void initRecycler(){
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.addItemDecoration(decoration);
+        recyclerView.setAdapter(adapter);
     }
 }
